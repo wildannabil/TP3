87f0054165b1a0335ac462419f0e8b152ba81027
wildan <wildannabil101@gmail.com>
2025-03-03 15:12:58 +0700

    Update fitur

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,11 @@ namespace TP3
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nama = textBox1.Text;
+            label1.Text = "Halo, " + nama;
+        }
     }
 }
